91f7de0dbfb69443c31a5ae69f0ad5f1e4f8b67a
Razvan Toma <razvantoma1573@gmail.com>
2020-07-22 10:11:40 +0300

    Map Based Services. closes #6

---
+++ pet-clinic-data/src/main/java/razvan/toma/petclinic/Service/Map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package razvan.toma.petclinic.Service.Map;
+
+import razvan.toma.petclinic.Model.Owner;
+import razvan.toma.petclinic.Service.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
