be26ba184b48a2686253bf9144ae4a3e553bdf4f
Razvan Toma <razvantoma1573@gmail.com>
2020-07-23 08:55:17 +0300

    Added Pet Type data on Bootstrap Data Loader. closes #32

--- pet-clinic-web/src/main/java/razvan/toma/petclinic/Bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/razvan/toma/petclinic/Bootstrap/DataLoader.java
@@ -3,8 +3,10 @@ package razvan.toma.petclinic.Bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import razvan.toma.petclinic.Model.Owner;
+import razvan.toma.petclinic.Model.PetType;
 import razvan.toma.petclinic.Model.Vet;
 import razvan.toma.petclinic.Service.OwnerService;
+import razvan.toma.petclinic.Service.PetTypeService;
 import razvan.toma.petclinic.Service.VetService;
 
 @Component
@@ -12,15 +14,29 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        // Pet types
+        PetType dog = new PetType();
+        dog.setName("Dog");
+
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+
+        PetType savedCatPetType = petTypeService.save(dog);
+
+        // Owners
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
@@ -35,6 +51,7 @@ public class DataLoader implements CommandLineRunner {
 
         System.out.println("Loaded Owners ... ");
 
+        // Vets
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
