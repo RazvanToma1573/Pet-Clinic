2f8e38069edf1c363315086aeaa215ebac377b9f
Razvan Toma <razvantoma1573@gmail.com>
2020-07-23 08:39:08 +0300

    Added Speciality Entity, added it to Vet. closes #27

---
+++ pet-clinic-data/src/main/java/razvan/toma/petclinic/Model/Speciality.java
@@ -0,0 +1,14 @@
+package razvan.toma.petclinic.Model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
