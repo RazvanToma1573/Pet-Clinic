d6ff2e07e78168e9f8f5e29ebe63d4067ffa685b
Razvan Toma <razvantoma1573@gmail.com>
2020-07-23 08:49:06 +0300

    Added Pet Type Map Service Impl. closes #29

---
+++ pet-clinic-data/src/main/java/razvan/toma/petclinic/Service/Map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package razvan.toma.petclinic.Service.Map;
+
+import org.springframework.stereotype.Service;
+import razvan.toma.petclinic.Model.PetType;
+import razvan.toma.petclinic.Service.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/razvan/toma/petclinic/Service/PetTypeService.java
@@ -0,0 +1,6 @@
+package razvan.toma.petclinic.Service;
+
+import razvan.toma.petclinic.Model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
