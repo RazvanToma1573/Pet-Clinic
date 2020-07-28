0678d2f420de45e1efc5ce1d9e36374f8b1143b0
Razvan Toma <razvantoma1573@gmail.com>
2020-07-28 10:38:44 +0300

    add or update pet. #56

--- pet-clinic-web/src/test/java/razvan/toma/petclinic/Controller/PetControllerTest.java
+++ pet-clinic-web/src/test/java/razvan/toma/petclinic/Controller/PetControllerTest.java
@@ -2,8 +2,10 @@ package razvan.toma.petclinic.Controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import razvan.toma.petclinic.Model.Owner;
@@ -21,6 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ExtendWith(MockitoExtension.class)
 class PetControllerTest {
 
     @Mock
